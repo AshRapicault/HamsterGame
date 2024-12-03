a8aeaa3af37530d643ae9d12f83bdd49c023b6a8
AshRapicault <aaliyah.rapicault@ll.sintgregorius.be>
2024-12-03 22:13:07 +0100

    animation fixed+ ground sprite added

--- HamsterGame/Assets/Scripts/PlayerMovement.cs
+++ HamsterGame/Assets/Scripts/PlayerMovement.cs
@@ -16,13 +16,13 @@ public class PlayerMovement : MonoBehaviour
         body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
         //the way the hamster is facing when moving left/right
-        if (horizontalInput > 0.01f)
+        if (horizontalInput > 0.1f)
         {
             transform.localScale = new Vector2(2,2);
         }
-        else if(horizontalInput < 0.01f)
+        else if(horizontalInput < 0.0f)
         {
-            transform.localScale = new Vector2(-2, 2);
+            transform.localScale = new Vector2(-2,2);
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
