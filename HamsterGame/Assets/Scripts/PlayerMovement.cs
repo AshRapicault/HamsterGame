a811af6887cbce4d9e02387822310b964c33b1a2
AshRapicault <aaliyah.rapicault@ll.sintgregorius.be>
2024-12-04 11:52:45 +0100

    jumping animation added + fixed mistakes

--- HamsterGame/Assets/Scripts/PlayerMovement.cs
+++ HamsterGame/Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,14 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
 
     private void Awake()
     {
+        // Get references for your components
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -15,18 +19,41 @@ public class PlayerMovement : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
-        //the way the hamster is facing when moving left/right
+        // The way the hamster is facing when moving left/right
         if (horizontalInput > 0.1f)
         {
-            transform.localScale = new Vector2(2,2);
+            transform.localScale = new Vector2(2, 2);  // Facing right
         }
-        else if(horizontalInput < 0.0f)
+        else if (horizontalInput < -0.1f)
         {
-            transform.localScale = new Vector2(-2,2);
+            transform.localScale = new Vector2(-2, 2); // Facing left
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
         {
-            body.velocity = new Vector2(body.velocity.x, speed);
+            Jump();
+        }
+
+        // Set walking animation parameter
+        anim.SetBool("walking", horizontalInput == 0);
+
+        // Set jumping animation parameter
+        anim.SetBool("jumping", !grounded); // Player is jumping if not grounded
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        grounded = false;
+        anim.SetBool("grounded", grounded);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            grounded = true;
+            anim.SetBool("grounded", grounded);
         }
     }
 }
